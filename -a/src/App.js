fdb713a881c539624efb75450b6989c0a9e336e8
Ray Huang <rayhuang.cj@gmail.com>
2020-01-20 23:09:49 -0800

    website prototype started #1

---
+++ -a/src/App.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="fLow">
+      {/*<header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>*/}
+      <header className="App-header">
+        <p>
+          Creative Lab: fLow
+        </p>
+
+        <a
+          className="github-link"
+          href="https://github.com/allisonchen23/fLOW"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Our GitHub
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
