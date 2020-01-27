90efa055987fd5714688edbde69570ab36b93a5c
Ray Huang <rayhuang.cj@gmail.com>
2020-01-26 17:54:43 -0800

    update 1.1

--- Interface/src/App.js
+++ Interface/src/App.js
@@ -1,40 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Template from './graphs/Template';
 import './App.css';
 
+class App extends Component {
+  render() {  
+    return (
+
+		<React.Fragment>
+      <Template/>
+    </React.Fragment>
+    );
+  }
+}
+{/*
 function App() {
   return (
-    <div className="fLow">
-      {/*<header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>*/}
-      <header className="App-header">
-        <p>
-          Creative Labs: fLow
-        </p>
-
-        <a
-          className="github-link"
-          href="https://github.com/allisonchen23/fLOW"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Our GitHub
-        </a>
-      </header>
-    </div>
+    
   );
 }
+*/}
 
 export default App;
